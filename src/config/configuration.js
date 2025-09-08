ee7557211923cce8438ea2fb64127f64ed6c9d40
Naeem Akhter <dreamabroad83@gmail.com>
2025-09-08 23:07:00 +0530

    issue fixed

--- src/config/configuration.js
+++ src/config/configuration.js
@@ -6,7 +6,7 @@ export const BASE_URL =
 export const PERSONAL_API_KEY =
   import.meta.env.VITE_DEVELOPMENT_TYPE === "development"
     ? import.meta.env.VITE_DEV_PERSONAL_API_KEY
-    : import.meta.env.VITE_PROD_PERSONAL_API_KEY;
+    : import.meta.env.VITE_PROD_PERSONAL_API_KEY; 
 
 export const VITE_DEV_TYNYMCE_API = import.meta.env.VITE_DEVELOPMENT_TYPE === "development"
   ? import.meta.env.VITE_DEV_TYNYMCE_API
